bc7e1537752722172a5cca14e6755db7f129cd73
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2023-10-17 15:40:03 +0530

    add seeding, analytics

---
+++ prisma/seed.js
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  await prisma.gender.upsert({
+    where: { name: 'Male' },
+    update: {},
+    create: {
+      name: 'Male',
+      code: 'M',
+    },
+  })
+  await prisma.gender.upsert({
+    where: { name: 'Female' },
+    update: {},
+    create: {
+      name: 'Female',
+      code: 'F',
+    },
+  })
+  await prisma.language.upsert({
+    where: { name: 'English' },
+    update: {},
+    create: {
+      name: 'English',
+    },
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import Header from './Header.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="app">
