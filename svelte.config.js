c544e3eda5556923400ac1044bb6b8df684264e6
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2024-04-03 20:21:48 +0530

    fix: adapt for railway, test

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
